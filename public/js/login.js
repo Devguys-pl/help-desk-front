988856059bbf965511da2ff2138cb88fc6d85abb
Xnsky <arturnawrocki121@gmail.com>
2021-04-08 22:40:37 +0200

    add forEach instead of for

--- public/js/login.js
+++ public/js/login.js
@@ -16,17 +16,34 @@ function getInfo() {
     let username = document.getElementById('username').value
     let password = document.getElementById('password').value
 
-    for (let i = 0; i < users.length; i++) {
-        if (username == users[i].username && password == users[i].password) {
+    //=================== WAY WITH forEach ========================
+
+    users.forEach(user => {
+    if (username == user.username && password == user.password) {
             info.textContent = (username + " is logged in")
             let logged = true;
-
 
             if (logged = true) {
                 content.textContent = ("Successful login, you can see this string now.")
             }
             return
-        }
-    }
+        }   
     info.textContent = ("incorrect username or password")
+});
+
+
+//================== WAY WITH for ==========================
+
+    // for (let i = 0; i < users.length; i++) {
+    //     if (username == users[i].username && password == users[i].password) {
+    //         info.textContent = (username + " is logged in")
+    //         let logged = true;
+
+    //         if (logged = true) {
+    //             content.textContent = ("Successful login, you can see this string now.")
+    //         }
+    //         return
+    //     }
+    // }
+    // info.textContent = ("incorrect username or password")
 }
