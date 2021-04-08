2bf6f3317b366c6e8a1be1a819ce4af973eb178c
shyBBy <edekkredek1910@gmail.com>
2021-04-08 14:08:34 +0200

    Alpha 0.0.1 - ready to start work.

---
+++ public/js/login.js
@@ -0,0 +1,32 @@
+let users = [{
+        username: "Artur",
+        password: "artur"
+    },
+    {
+        username: "Dawid",
+        password: "dawid"
+    }
+]
+let logged = false;
+
+let info = document.getElementById('info');
+let content = document.getElementById('success')
+
+function getInfo() {
+    let username = document.getElementById('username').value
+    let password = document.getElementById('password').value
+
+    for (let i = 0; i < users.length; i++) {
+        if (username == users[i].username && password == users[i].password) {
+            info.textContent = (username + " is logged in")
+            let logged = true;
+
+
+            if (logged = true) {
+                content.textContent = ("Successful login, you can see this string now.")
+            }
+            return
+        }
+    }
+    info.textContent = ("incorrect username or password")
+}
