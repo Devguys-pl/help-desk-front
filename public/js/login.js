b9486c50387e3ec04a805aa980e4752d73cf6580
Xnsky <arturnawrocki121@gmail.com>
2021-04-15 02:13:37 +0200

    Almost finished styling with bootstrap

--- public/js/login.js
+++ public/js/login.js
@@ -5,6 +5,10 @@ let users = [{
     {
         username: "Dawid",
         password: "dawid"
+    },
+    {
+        username: "jaro",
+        password: "jaro"
     }
 ]
 let logged = false;
@@ -12,12 +16,12 @@ let logged = false;
 let info = document.getElementById('info');
 let content = document.getElementById('success')
 
-function getInfo(e) {
+function getInfo() {
     // e.preventDefault();
     let username = document.getElementById('username').value
     let password = document.getElementById('password').value
-    users.forEach(user => {
-    if (username == user.username && password == user.password) {
+    for (let i = 0; i < users.length; i++) {
+        if (username == users[i].username && password == users[i].password) {
             info.textContent = (username + " is logged in")
             let logged = true;
 
@@ -25,9 +29,26 @@ function getInfo(e) {
                 content.textContent = ("Successful login, you can see this string now.")
             }
             return
-        }   
+        }
+    }
     info.textContent = ("incorrect username or password")
-});
+
+
+
+//  i went back to the for loop because there were problems witch forEach
+
+//     users.forEach(user => {
+//     if (username == user.username && password == user.password) {
+//             info.textContent = (username + " is logged in")
+//             let logged = true;
+
+//             if (logged = true) {
+//                 content.textContent = ("Successful login, you can see this string now.")
+//             }
+//             return
+//         }   
+//     info.textContent = ("incorrect username or password")
+// });
 }
 
 function onSignIn(googleUser) {
@@ -36,4 +57,15 @@ function onSignIn(googleUser) {
   console.log('Name: ' + profile.getName());
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+let id_token = googleUser.getAuthResponse().id_token; // ID token for backend
+        console.log("ID Token: " + id_token);
 }
+
+
+
+
+
+
+
+
