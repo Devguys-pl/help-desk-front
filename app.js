8ffd4038cd6eb4350df948bcd268481aba4ed081
shyBBy <edekkredek1910@gmail.com>
2021-04-20 13:05:38 +0200

    Rebuild app.js

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+var port = 3000;
+
+app.listen(port, function() {
+    console.log('app started');
+  });
 
 
 app.use(logger('dev'));
@@ -18,27 +24,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.json;
-});
-
-app.get('/hello/:name', function (req, res) {
-  res.send('Hello World!' + req.params.name)
-})
-
-
 
 module.exports = app;
