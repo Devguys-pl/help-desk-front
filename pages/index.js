0d515e9a300061a82db7abfabcd59b2c7e2990e1
shyBBy <edekkredek1910@gmail.com>
2021-05-09 16:05:57 +0200

    Added mdb-ui v0.0.3a

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
--- pages/login/index.js
+++ pages/login/index.js
@@ -1,6 +1,9 @@
+import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
+import React from 'react';
+import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBBtn } from 'mdb-react-ui-kit';
 
 
 export default function Login() {
@@ -12,6 +15,15 @@ export default function Login() {
             <a>Back to home</a>
             </Link>
         </div>
+            <MDBCard style={{ maxWidth: '22rem' }}>
+                <MDBCardBody>
+                    <MDBCardTitle>Card title</MDBCardTitle>
+                    <MDBCardText>
+                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                    </MDBCardText>
+                    <MDBBtn>Button</MDBBtn>
+                </MDBCardBody>
+        </MDBCard>
       </>
   )
 }
