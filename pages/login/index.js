7f5276c198c31a6b9a145058559a7240faa6cf08
shyBBy <edekkredek1910@gmail.com>
2021-05-10 19:54:29 +0200

    added own styles file

--- pages/login/index.js
+++ pages/login/index.js
@@ -4,26 +4,15 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react';
 import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBBtn } from 'mdb-react-ui-kit';
+import styles from '/styles/Home.module.css'
 
 
-export default function Login() {
+export default function Login(props) {
   return (
       <>
-        <h1> Hello devguys this is /login</h1>
-        <div>
-            <Link href="/">
-            <a>Back to home</a>
-            </Link>
+        <div className={styles.background}>
+            Test
         </div>
-            <MDBCard style={{ maxWidth: '22rem' }}>
-                <MDBCardBody>
-                    <MDBCardTitle>Card title</MDBCardTitle>
-                    <MDBCardText>
-                        Some quick example text to build on the card title and make up the bulk of the card's content.
-                    </MDBCardText>
-                    <MDBBtn>Button</MDBBtn>
-                </MDBCardBody>
-        </MDBCard>
       </>
   )
 }
