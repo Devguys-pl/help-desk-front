497c324aba5a29d5af69eca645f9116028c864c7
shyBBy <edekkredek1910@gmail.com>
2021-05-10 21:15:39 +0200

    Login page in progress

--- pages/login/index.js
+++ pages/login/index.js
@@ -3,16 +3,36 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react';
-import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBBtn } from 'mdb-react-ui-kit';
 import styles from '/styles/Home.module.css'
+import { MDBContainer, MDBRow, MDBCol } from 'mdb-react-ui-kit';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 
 export default function Login(props) {
   return (
       <>
-        <div className={styles.background}>
-            Test
-        </div>
+            <MDBContainer>
+                <div className={styles.container}>
+                    <MDBRow>
+                        <MDBCol size='md' className='col-example'>
+                            <img
+                            src='https://dz2cdn1.dzone.com/storage/temp/12334613-971.jpg'
+                            className='picture'
+                            alt='...'
+                            style={{ maxWidth: '67vh' }}
+                            />
+                        </MDBCol>
+                        <MDBCol size='md' className='col-example'>
+                                LOGOWANKO
+                        </MDBCol>
+                    </MDBRow>
+                    Go to {' '}
+                    <Link href="/">
+                        <a className='text-danger'> Main page</a>
+                    </Link>
+                </div>
+            </MDBContainer>
       </>
   )
 }
